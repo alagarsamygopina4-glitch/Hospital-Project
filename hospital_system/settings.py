8f29e01ad245322001671f3ec2f276aed806c9b8
Gopina <alagarsamygopina4@gmail.com>
2026-03-17 11:18:23 +0530

    Added requirements and production settings

--- hospital_system/settings.py
+++ hospital_system/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_pk51@tk#gcmury$w*wgwv#75h1cd_6fu@*-1rlj#d$*(ojina'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-_pk51@tk#gcmury$w*wgwv#75h1cd_6fu@*-1rlj#d$*(ojina')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['10.64.152.241', '10.64.152.17', '127.0.0.1', 'localhost', '*']
+ALLOWED_HOSTS = ['10.64.152.241', '10.64.152.17', '127.0.0.1', 'localhost', '*.onrender.com']
 
 
 # Application definition
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -158,6 +160,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
